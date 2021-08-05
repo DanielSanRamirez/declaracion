69fb21c90f5dd99484de3c5b9075d7442b081693
DanielSanRamirez <danielsanramirez@hotmail.com>
2021-08-05 11:42:52 -0500

    change en width image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import manHello from './assets/images/hombre.png'
+import manHello from './assets/images/hombre2.png'
 import './App.css';
 import { CardInformation } from './shared/card-information/CardInformation';
 
 function App() {
 
   const propCardInformation = {
-    urlImg: 'https://ouch-cdn2.icons8.com/fC0jJAKzloPQ3aHDc5PKdaL6mcCRt7cHIZq_jNNKlUA/rs:fit:1207:912/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTQ5/L2Q2OTk3MGMxLWZm/NGYtNDc1NS1hYjAz/LWEwZmY0ZDE0Nzli/OS5wbmc.png',
+    urlImg: manHello,
     text: 'Hola como estás mucho gusto'
   } 
 
